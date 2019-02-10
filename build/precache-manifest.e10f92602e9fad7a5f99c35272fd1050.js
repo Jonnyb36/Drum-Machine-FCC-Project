9405070dbfbcf3f127ca242284a3a8cfe4154c1f
Jon Brown <jonnyb36@hotmail.com>
2019-02-10 17:11:48 +0000

    Add gh-pages build

---
+++ build/precache-manifest.e10f92602e9fad7a5f99c35272fd1050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7aeb7b53f9a1c9d32a",
+    "url": "/Drum-Machine-FCC-Project/static/js/runtime~main.0e7aeb7b.js"
+  },
+  {
+    "revision": "63a01da86406e7d7a05b",
+    "url": "/Drum-Machine-FCC-Project/static/js/main.63a01da8.chunk.js"
+  },
+  {
+    "revision": "c1fe15a5f9521d404186",
+    "url": "/Drum-Machine-FCC-Project/static/js/1.c1fe15a5.chunk.js"
+  },
+  {
+    "revision": "63a01da86406e7d7a05b",
+    "url": "/Drum-Machine-FCC-Project/static/css/main.9b0370be.chunk.css"
+  },
+  {
+    "revision": "c1fe15a5f9521d404186",
+    "url": "/Drum-Machine-FCC-Project/static/css/1.80ada959.chunk.css"
+  },
+  {
+    "revision": "05a6e789afc7047f967111d1153fd602",
+    "url": "/Drum-Machine-FCC-Project/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Drum-Machine-FCC-Project/precache-manifest.e10f92602e9fad7a5f99c35272fd1050.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Drum-Machine-FCC-Project/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
